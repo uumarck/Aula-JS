33d3652cdd5a4b7a42d58747f541679b010e7706
marckanderson <2mandersonb@gmail.com>
2024-08-02 15:29:40 -0300

    aula03

---
+++ aula03.js
@@ -0,0 +1,31 @@
+/* arquivo JS Operadores e Operações */
+let n1= 10    // number inteiro
+let n2= 2     // number inteiro
+let n3= "2"   // string texto
+
+document.write(´N1 ${n1} <br>´)
+document.write(´N2 ${n2} <br>´)
+document.write(´N3 ${n3} <br>´)
+
+console.log(n1 + n2) // adicao
+console.log(n1 - n2) // subtracao
+console.log(n1 * n2) // multiplicacao
+console.log(n1 / n2) //divisao
+console.log(n1 % n2) //resto ou modulo
+
+console.log(n2 == n3)  // igual a
+console.log(n2 === n3) // identico a
+console.log(n2 != n3)  // diferente de nao=
+console.log(n2 !== n3) // Nao e identico
+
+console.log(n1 > n2)              // maior que
+console.log(n1 < n2)              // menor que
+console.log(n1 >= n2)             // maior ou igual a
+console.log(n1 <= n2)             // menor ou igual a
+
+let a = true
+let b = false
+console.log (a && b)    // E   AND
+console.log(a || b)     // OU  OR
+console.log(!a)         // NOT NAO negacao
+console.log(!b)
